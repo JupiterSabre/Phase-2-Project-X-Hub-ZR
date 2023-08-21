d256002079eaad0813db12422cb8eb67ecf5f086
Zulai Romero <zulairomero@gmail.com>
2023-08-21 17:31:49 -0400

    Configured calendar, added routes to signup sheet

--- src/Components/RCalendar.jsx
+++ src/Components/RCalendar.jsx
@@ -1,18 +1,36 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
+import { useNavigate } from "react-router-dom"
 // import 'react-calendar/dist/Calendar.css';
 
+function RCalendar() {
+    const navigate = useNavigate()
 
-const RCalendar = () => {
-    const [date, setDate] = useState(new Date())
-    const onChange = () => {
+    const [date, setDate] = useState(new Date());
+    const onChange = date => {
         setDate(date)
     }
+
+    const clickedDay = value => {
+        const currentDate = new Date();
+        const twoWeeksLater = new Date();
+        twoWeeksLater.setDate(currentDate.getDate() + 5
+        )
+        if (value < currentDate) {
+        alert('This date is in the past, please select the current or future date to book an appointment.');
+        } else if (value <= twoWeeksLater) {
+            alert('You are being redirected to a ticket portal for short-term passes');
+            window.location.href="https://xxxxxxxxxx.app/hxnyc";
+        } else {alert("Join the fray for access to future programming =) ");
+        navigate('/')}
+        }
+    
 
 
     return <div>
 
-        <Calendar onChange={onChange} className="react-calendar" value={date} />
+        <Calendar onChange={onChange} className="react-calendar" value={date} onClickDay={clickedDay} />
+        {console.log(date)}
     </div>
 }
 
